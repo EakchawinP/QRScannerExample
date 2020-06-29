90afec962503daff9e669d51c6caa2c08395ea9e
SmartAOFsDev <delelop.mistine@gmail.com>
2020-06-29 11:08:49 +0700

    Initial Commit

---
+++ QRScannerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRScannerExample
+//
+//  Created by SD-M004 on 29/6/2563 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
