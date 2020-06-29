badcb40f0268070c2adb14b4fd66c1603fcc3881
SmartAOFsDev <delelop.mistine@gmail.com>
2020-06-29 13:39:55 +0700

    Upload Code

---
+++ QRScannerExample/QRScannerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QRScannerViewController.swift
+//  QRScannerExample
+//
+//  Created by SD-M004 on 29/6/2563 BE.
+//
+
+import UIKit
+
+class QRScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
